654c7af0fdd1b853b116847499dda58ce9436765
Nilender-Andrey <nilenderav@mail.ru>
2022-01-04 07:18:32 +0400

    feat: add new solution

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-module.exports = function reverse (n) {
-  
-  n = Math.abs(n).toString();
-  
-  let newStr = "";
- 
-  for(let i = n.length-1; i>=0; i--) {
-    newStr += n[i];
-    
-  }
-
-  return +newStr;
- 
-}
- 
+module.exports = function reverse(n) {
+    return parseInt(n.toString().split("").reverse().join(""));
+};
